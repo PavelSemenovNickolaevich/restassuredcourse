a7220389eb700df6abed809ed897e548cfb47055
test <natitshow@gmail.com>
2020-05-14 20:41:23 +0300

    //Gpath Json Part2 - Using findAll to extract multiple data

--- src/test/java/GPathJSONTests.java
+++ src/test/java/GPathJSONTests.java
@@ -2,6 +2,7 @@ import config.TestConfig;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.get;
@@ -13,9 +14,29 @@ public class GPathJSONTests extends TestConfig {
     public void extractMapOfElementsWithFind(){
 
         Response response = get("/competitions/2000/teams");
-//Поиск элемента teams , затем в тиамс элемента tla c BRA
+//Поиск элемента teams , затем в тиамс элемента tla c BRA на языке груви
         Map<String, ?> allTeamDataForSingleTeam = response.path
                 ("teams.find{it.tla == 'BRA'}");
         System.out.println(allTeamDataForSingleTeam);
+    }
+
+    //Gpath Json Part2 - Using findAll to extract multiple data
+    @Test
+    public  void extractSingleValueWithFind() {
+
+        Response response = get("teams/18/");
+        String nationality = response.
+                path("squad.find{it.name == 'Tobias Sippel'}.nationality");
+        System.out.println(nationality);
+    }
+
+    @Test
+    public  void extrctListOfValuesWithFindAll() {
+        Response response = get("teams/18/");
+        List<String> playersNationality = response.
+                path("squad.findAll{it.name != 'Tobias Sippel'}.nationality");
+        for (String allNationality : playersNationality) {
+            System.out.println(allNationality);
+        }
     }
 }
