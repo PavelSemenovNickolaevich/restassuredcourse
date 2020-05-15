2754595b24839b2118200249118d0c733c2f12e0
test <natitshow@gmail.com>
2020-05-15 17:05:57 +0300

    // auth

--- src/test/java/AuthenticationTests.java
+++ src/test/java/AuthenticationTests.java
@@ -6,10 +6,10 @@ import static io.restassured.RestAssured.given;
 
 public class AuthenticationTests {
 
-    @BeforeClass
-    public static  void setup() {
- //       RestAssured.proxy("localhost", 8888);
-    }
+//    @BeforeClass
+//    public static  void setup() {
+//   //     RestAssured.proxy("localhost", 8888);
+//    }
 //basic auth
     @Test
     public void basicPreemtiveAuthTest() {
@@ -21,6 +21,32 @@ public class AuthenticationTests {
     @Test
     public void  basicChallengeAuthTest() {
         given().auth().basic("username", "password").
+                when().get("http://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public void  oauth1Test() {
+        given().auth().
+                oauth("consumerKey", "consumerSecret","consumerAccessToken",  "secretToken").
+        when().get("http://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public void  oauth2Test() {
+        given().auth().
+                oauth2("accessToken").
+                when().get("http://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public  void relaxedHTTPSTest() {
+        given().relaxedHTTPSValidation().
+                when().get("http://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public  void keyStoreTest() {
+        given().keyStore("/pathToJKS", "password").
                 when().get("http://localhost:8080/someEndpoint");
     }
 }
